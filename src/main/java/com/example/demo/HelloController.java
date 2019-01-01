11c060223cb49747f35e1d348b478d37b69d0a55
Mohanraj Thirumoorthy <mohanrajt@yahoo.com>
2019-01-01 15:13:33 -0500

    Test message

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Test Message. Greetings from Spring Boot!";
     }
 
 }
